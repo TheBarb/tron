b802b8bb929202f9f1b914f6b3816ff3502e6df8
mail@studio7berlin.de <mailstudio7berlin.de@mailstuindesMBP.fritz.box>
2021-03-08 08:13:49 +0100

    js basic functionality

---
+++ functions.ts
@@ -0,0 +1,77 @@
+let grid = {
+    startX: 0,
+    startY: 0,
+    endX: 99,
+    endY: 99
+}
+
+let tron = {
+    direction: "O",
+    x: 1,
+    y: 49,
+    travelLog: []
+}
+
+function turnLeft(tron) {
+    if (tron.direction === "N") {
+        tron.direction = "W"
+    } else if (tron.direction === "O") {
+        tron.direction = "N"
+    } else if (tron.direction === "S") {
+        tron.direction = "O"
+    } else if (tron.direction === "W") {
+        tron.direction = "S"
+    }
+    console.log(tron);
+}
+
+function turnRight(tron) {
+    if (tron.direction === "N") {
+        tron.direction = "O"
+    } else if (tron.direction === "O") {
+        tron.direction = "S"
+    } else if (tron.direction === "S") {
+        tron.direction = "W"
+    } else if (tron.direction === "W") {
+        tron.direction = "N"
+    }
+    console.log(tron);
+}
+
+
+function moveForward(tron) {
+    if (tron.direction === "N") {
+        tron.y -= 1;
+        if (tron.y <= 1 || tron.travelLog.indexOf(`${tron.x},${tron.y}`) > -1) {
+            console.log('Pow');
+        } else {
+            tron.travelLog.push(`${tron.x},${tron.y}`)
+        }
+    } else if (tron.direction === "O") {
+        tron.x += 1;
+        if (tron.x >= 99 || tron.travelLog.indexOf(`${tron.x},${tron.y}`) > -1) {
+            console.log('Pow');
+        } else {
+            tron.travelLog.push(`${tron.x},${tron.y}`)
+        }
+    } else if (tron.direction === "S") {
+        tron.y += 1;
+        if (tron.y >= 99 || tron.travelLog.indexOf(`${tron.x},${tron.y}`) > -1) {
+            console.log('Pow');
+        } else {
+            tron.travelLog.push(`${tron.x},${tron.y}`)
+        }
+    } else if (tron.direction === "W") {
+        tron.x -= 1;
+        if (tron.x <= 1 || tron.travelLog.indexOf(`${tron.x},${tron.y}`) > -1) {
+            console.log('Pow');
+        } else {
+
+            tron.travelLog.push(`${tron.x},${tron.y}`)
+        }
+    } console.log(tron)
+}
+
+//moveForward(tron);
+//turnRight(tron);
+//turnLeft(tron);
